202c83d3636d07c8e814c55d3ec990103de6ff48
Davi Volcan <davi.volcan2@gmail.com>
2024-09-16 18:11:36 -0300

    deixa mais com cara de humano

--- JACOBI/jacobi.py
+++ JACOBI/jacobi.py
@@ -11,16 +11,14 @@ X = np.array([0.7, -1.6, 0.6])
 # vetor b
 B = np.array([7, -8, 6])
 
-# código
 n = 3
 k = 1
-max_iter = 4  # número máximo de iterações
+max_iter = 4  
 tol = 5
 
-D = np.zeros((n, n))  # Inicializa matriz D
-I = np.zeros(n)  # Inicializa vetor I
+D = np.zeros((n, n))  
+I = np.zeros(n)  
 
-# Loop para calcular D e I
 for i in range(n):
     for j in range(n):
         if i == j:
@@ -29,12 +27,11 @@ for i in range(n):
         else:
             D[i, j] = -A[i, j] / A[i, i]
 
-# Iteração para atualizar X
 while k < max_iter:
     X1 = np.dot(D, X) + I
 
-    err = np.abs((X1 - X) / X1) * 100  # Calcula erro percentual
-    mtol = np.full(n, tol)  # Vetor de tolerância
+    err = np.abs((X1 - X) / X1) * 100  #Calcula erro percentual
+    mtol = np.full(n, tol)  #Vetor de tolerância
 
     # Verifica se o erro é menor que a tolerância para todos os elementos
     if np.all(err < mtol):
